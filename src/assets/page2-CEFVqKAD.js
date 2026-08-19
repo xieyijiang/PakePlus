35c668884a480506cad64a0d2939fe0d52b85427
Crabby <390094305@qq.com>
2026-08-19 15:26:38 +0800

    update src file from pakeplus

---
+++ src/assets/page2-CEFVqKAD.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+  
+import{E as e,Ft as t,It as n,Mt as r,Pt as i,U as a,xt as o}from"./_virtual_mf___mfe_internal__moth_mf_2_admin_mf_2_core_mf_2_naive__mf_owner__1__loadShare__pinia__loadShare__.js-BDuzYzk1.js";import{S as s,ot as c}from"./components-D2Ept3Al.js";import{t as l}from"./_plugin-vue_export-helper-B7aGp3iI.js";var u=n({__name:`page2`,setup(e,{expose:t}){t();let{t:n}=c(),r={t:n};return Object.defineProperty(r,`__isScriptSetup`,{enumerable:!1,value:!0}),r}});function d(n,c,l,u,d,f){let p=s;return e(),r(`div`,null,[t(p,null,{default:a(()=>[i(o(u.t(`route.multimenu.level2.level3.page2`)),1)]),_:1})])}var f=l(u,[[`render`,d],[`__file`,`/Users/crab7/develop/moth-admin-core-pro/apps/moth-admin-core-naive/src/views/multilevel_menu_example/level2/level3/page2.vue`]]);export{f as default};
