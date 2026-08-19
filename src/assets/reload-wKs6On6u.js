f786b34245d86bfefa228793b73514ceca7fe2d6
Crabby <390094305@qq.com>
2026-08-19 15:46:21 +0800

    update src file from pakeplus

---
+++ src/assets/reload-wKs6On6u.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+  
+import{C as e,E as t,It as n,Mt as r}from"./_virtual_mf___mfe_internal__moth_mf_2_admin_mf_2_core_mf_2_naive__mf_owner__1__loadShare__pinia__loadShare__.js-BDuzYzk1.js";import{t as i}from"./_plugin-vue_export-helper-B7aGp3iI.js";import{n as a}from"./_virtual_mf___mfe_internal__moth_mf_2_admin_mf_2_core_mf_2_naive__mf_owner__1__loadShare__vue_mf_2_router__loadShare__.js-CO6HiyyD.js";var o=n({__name:`reload`,setup(t,{expose:n}){n();let r=a();e(()=>{r.go(-1)});let i={router:r};return Object.defineProperty(i,`__isScriptSetup`,{enumerable:!1,value:!0}),i}});function s(e,n,i,a,o,s){return t(),r(`div`)}var c=i(o,[[`render`,s],[`__file`,`/Users/crab7/develop/moth-admin-core-pro/apps/moth-admin-core-naive/src/views/reload.vue`]]);export{c as default};
