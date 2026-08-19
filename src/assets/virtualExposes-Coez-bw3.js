cc2bc10c879dae4e1b33f890e031e4b8c255f4f3
Crabby <390094305@qq.com>
2026-08-19 15:29:27 +0800

    update src file from pakeplus

---
+++ src/assets/virtualExposes-Coez-bw3.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+  
+import{t as e}from"./virtual_mf-exposes___mfe_internal__moth-admin-core-naive__remoteEntry-_hash_-Bk_YeCDK.js";export{e as default};
