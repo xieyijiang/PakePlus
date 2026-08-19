aab9f01630f184be0f40c13d439c5efae07b6ef8
Crabby <390094305@qq.com>
2026-08-19 15:27:50 +0800

    update src file from pakeplus

---
+++ src/assets/remoteEntry-D9JWSdyd.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+  
+import{n as e,t}from"./virtual_mf-REMOTE_ENTRY_ID___mfe_internal__moth-admin-core-naive__remoteEntry-_hash_-ByDO9uYw.js";export{t as get,e as init};
